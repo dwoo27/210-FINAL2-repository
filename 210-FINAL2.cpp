102caf8ed773d3412ca4631928d8a34bf8ae7aaa
dwoo27 <dwooed27@gmail.com>
2026-05-20 19:50:31 -0700

    10 min commit

--- 210-FINAL2.cpp
+++ 210-FINAL2.cpp
@@ -6,24 +6,44 @@
 using namespace std;
 
 const int PERIODS = 10;
+const int DATA = 25;
 
-struct Node {
+struct Customer { //general struct for customer to be used for all booths
+	string name;
+	string order;
+};
+
+struct Node { //struct for nodes in coffee booth
 	Customer c;
 	Node* next;
 };
-struct Customer {
-	string name;
-	string order
-};
 
-struct coffeeLine {
+struct CoffeeLine { //struct for linked list for coffee booth
 	Node* head;
 	Node* tail;
+	int count;
 };
 
 int main()
 {
+	//data generated using LLM
+	string names[DATA] = {
+	"Alex", "Jordan", "Taylor", "Morgan", "Casey",
+	"Riley", "Jamie", "Avery", "Cameron", "Skyler",
+	"Reese", "Dakota", "Quinn", "Parker", "Rowan",
+	"Emerson", "Finley", "Hayden", "Kendall", "Logan",
+	"Bailey", "Harper", "Payton", "Sawyer", "Tatum"
+	};
 
+	string drinks[DATA] = {
+	"Latte", "Mocha", "Americano", "ColdBrew", "Espresso",
+	"Cappuccino", "Macchiato", "Chai", "DripCoffee", "IcedCoffee",
+	"FlatWhite", "Cortado", "NitroColdBrew", "VanillaLatte", "CaramelMacchiato",
+	"HazelnutLatte", "WhiteMocha", "MatchaLatte", "DirtyChai", "CafeAuLait",
+	"RedEye", "Affogato", "IcedAmericano", "HoneyLatte", "PumpkinLatte"
+	};
+
+	CoffeeLine coffeeLine;
 }
 
 
