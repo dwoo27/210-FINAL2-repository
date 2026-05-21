f998fc357c766830e0594f3d0092b4377cc3b75a
dwoo27 <dwooed27@gmail.com>
2026-05-20 19:41:41 -0700

    10 min commit

--- 210-FINAL2.cpp
+++ 210-FINAL2.cpp
@@ -1,6 +1,25 @@
-
-
 #include <iostream>
+#include <string>
+#include <deque> //ds for muffins
+#include <vector> //ds for bracelets
+#include <queue> //ds for stickers
+using namespace std;
+
+const int PERIODS = 10;
+
+struct Node {
+	Customer c;
+	Node* next;
+};
+struct Customer {
+	string name;
+	string order
+};
+
+struct coffeeLine {
+	Node* head;
+	Node* tail;
+};
 
 int main()
 {
